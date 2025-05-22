586ac322fb868ac759dbb92535b59331508ff98d
Woojin Chae <woojeeny02@kaist.ac.kr>
2025-05-23 01:53:31 +0900

    remove

--- main.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-
-if __name__ == '__main__':
-    # for df in [getUserInfo(), getFcltyGrpInfo(), getCntrbtrInfo(), getRceptStat(), getProvdStat(), getCnttgInfo(), getSpctrInfo(), getPreferInfo()]:
-    #     print(df.head())
-    
-    df = getUserInfo()
-    print(df.head())
