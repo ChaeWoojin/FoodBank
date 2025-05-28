8ca3daf8d7a0b8e0a67d9c79370fdab58713bc0c
Woojin Chae <woojeeny02@kaist.ac.kr>
2025-05-28 21:50:38 +0900

    Feat: run

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+main_page = st.Page("./pages/main_page.py", title="메인", icon="🛒")
+page_1 = st.Page("./pages/page_1.py", title="푸드뱅크/마켓 물품 조회", icon="🏢")
+page_2 = st.Page("./pages/page_2.py", title="물품 공급 제안", icon="🔗")
+
+# Set up navigation 
+pg = st.navigation([main_page, page_1, page_2])
+st.set_page_config(layout="wide")
+
+sidebar_logo = "./logo/logo_sidebar.png"
+main_logo = "./logo/logo_main.png"
+st.logo(image=sidebar_logo, icon_image=main_logo, size='large')
+
+st.markdown("""
+    <style>
+    /* 사이드바 로고 이미지 */
+    [data-testid="stSidebar"] img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 70%;
+        height: auto;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+
+    # /* 메인 바디 헤더 로고 이미지 */
+    # img[data-testid="stLogo"] {
+    #     width: 4rem !important;
+    # }
+    </style>
+""", unsafe_allow_html=True)
+
+# Run the selected page
+pg.run()
+
+
